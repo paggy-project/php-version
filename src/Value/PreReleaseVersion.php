5a2e69401a3c010452186e36691d342b7aaae89b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-10-04 19:08:28 -0500

    Create PreReleaseVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/PreReleaseVersion.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\SemanticVersion\Value;
+
+final class PreReleaseVersion
+{
+    public function __construct(private string $value)
+    {
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
