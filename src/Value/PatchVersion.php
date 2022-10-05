aada304ee38f391c48beb997a70b93cdc1107b31
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-10-04 19:08:16 -0500

    Create PatchVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/PatchVersion.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\SemanticVersion\Value;
+
+final class PatchVersion
+{
+    public function __construct(private int $value)
+    {
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+}
