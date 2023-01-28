37b17c2635168f8a0e7a40e02597d38ea690c06a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-28 00:18:34 -0600

    Update Version.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Version.php
+++ src/Version.php
@@ -2,68 +2,108 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\SemanticVersion;
+namespace Ghostwriter\Version;
 
-use Ghostwriter\SemanticVersion\Value\BuildVersion;
-use Ghostwriter\SemanticVersion\Value\MajorVersion;
-use Ghostwriter\SemanticVersion\Value\MinorVersion;
-use Ghostwriter\SemanticVersion\Value\PatchVersion;
-use Ghostwriter\SemanticVersion\Value\PreReleaseVersion;
-
-final class Version
+final class Version implements VersionInterface
 {
-    private MajorVersion $major;
-
-    private MinorVersion $minor;
-
-    private PatchVersion $patch;
-
-    private PreReleaseVersion $prerelease;
-
-    private BuildVersion $build;
-
+    /**
+     * @param array<string> $prerelease
+     * @param array<string> $build
+     */
     public function __construct(
-        private string $value
+        private readonly int $major = 0,
+        private readonly int $minor = 0,
+        private readonly int $patch = 0,
+        private readonly array $prerelease = [],
+        private readonly array $build = []
     ) {
-        $this->major = new MajorVersion(0);
-        $this->minor = new MinorVersion(0);
-        $this->patch = new PatchVersion(0);
-        $this->prerelease = new PreReleaseVersion('');
-        $this->build = new BuildVersion('');
     }
 
-    public function getMajor(): MajorVersion
+    public function __toString(): string
     {
-        return $this->major;
+        return implode('', $this->jsonSerialize());
     }
 
-    public function getMinor(): MinorVersion
+    public function decrementMajor(): self
     {
-        return $this->minor;
+        return new self($this->major - 1, $this->minor, $this->patch, $this->prerelease, $this->build);
     }
 
-    public function getPatch(): PatchVersion
+    public function decrementMinor(): self
     {
-        return $this->patch;
+        return new self($this->major, $this->minor - 1, $this->patch, $this->prerelease, $this->build);
     }
 
-    public function getPrerelease(): PreReleaseVersion
+    public function decrementPatch(): self
     {
-        return $this->prerelease;
+        return new self($this->major, $this->minor, $this->patch - 1, $this->prerelease, $this->build);
     }
 
-    public function getBuild(): BuildVersion
+    public function getBuild(): array
     {
         return $this->build;
     }
 
-    public static function parse(string $version): self
+    public function getMajor(): int
     {
-        return new self($version);
+        return $this->major;
     }
 
-    public function getValue(): string
+    public function getMinor(): int
     {
-        return $this->value;
+        return $this->minor;
+    }
+
+    public function getPatch(): int
+    {
+        return $this->patch;
+    }
+
+    public function getPrerelease(): array
+    {
+        return $this->prerelease;
+    }
+
+    public function incrementMajor(): self
+    {
+        return new self($this->major + 1, 0, 0, $this->prerelease, $this->build);
+    }
+
+    public function incrementMinor(): self
+    {
+        return new self($this->major, $this->minor + 1, 0, $this->prerelease, $this->build);
+    }
+
+    public function incrementPatch(): self
+    {
+        return new self($this->major, $this->minor, $this->patch + 1, $this->prerelease, $this->build);
+    }
+
+    /**
+     * @return array{major:int,minor:int,patch:int,prerelease:string,build:string}
+     */
+    public function jsonSerialize(): array
+    {
+        /** @var null|array{major:int,minor:int,patch:int,prerelease:string,build:string} $data */
+        static $data = null;
+
+        return $data ??= [
+            'major' => $this->major,
+            'minor' => $this->minor,
+            'patch' => $this->patch,
+            'prerelease' => implode('.', $this->prerelease),
+            'build' => implode('.', $this->build),
+        ];
+    }
+
+    public static function parse(string $version): self
+    {
+        $grammar = new Grammar();
+
+        $lexer = new Lexer($grammar);
+
+        die(var_dump(iterator_to_array($lexer->lex($version))));
+
+        return new self();
     }
 }
